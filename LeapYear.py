fd9233331fa3b180571ada927f9cb55ad4c4cea7
charankumartv <charankumartv.ds@gmail.com>
2021-06-09 12:12:25 -0500

    LeapYear

--- LeapYear.py
+++ LeapYear.py
@@ -6,14 +6,18 @@ Given a year, determine whether it is a leap year. If it is a leap year, return 
 Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
 """
 def is_leap(year):
-    leap = False
     
     # Write your logic here
     if year % 4 == 0:
-        leap = True
+        if year % 100 == 0:
+            if year % 400 == 0:
+                leap = True
+            else:
+                leap = False 
+        else:
+            leap = True
     else:
-        leap = False
-    
+        leap = False       
     return leap
 
 year = int(input())
